a548b6d5155299822abb4c38a4312a940599e740
Karim-W <Karim.Wael@gmail.com>
2020-08-10 15:33:10 +0400

    update 2

--- Workout Timer/Controller/ViewController.swift
+++ Workout Timer/Controller/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
     var setMin = Int()
     var setSec = Int()
     let shapelayer = CAShapeLayer()
-    let center =  CGPoint(x: Int(UIScreen.main.bounds.width/2), y: 350)
+    let center =  CGPoint(x: Int(UIScreen.main.bounds.width/2), y:Int(UIScreen.main.bounds.height/2)) //y: 350)
     let Aftercenter =  CGPoint(x: Int(UIScreen.main.bounds.width/2), y: 350 + Int((UIScreen.main.bounds.width)/2 - 20))
     var audioPlayer = AVAudioPlayer()
     let goSound: SystemSoundID = 1030
@@ -25,6 +25,9 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
     var times = Double()
     var adder = ""
     var pressed = false
+    var setsToDo = Int()
+    var setsDone = Int()
+    var workout_started = false
 
     //MARK:PickerView stuff
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -74,6 +77,13 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
         t.translatesAutoresizingMaskIntoConstraints = false
         return t
     }()
+    var stopButton : UIButton = {
+        let t = UIButton()
+        t.setTitle("Stop", for: .normal)
+        t.setTitleColor(UIColor.systemBlue, for: .normal)
+        t.translatesAutoresizingMaskIntoConstraints = false
+        return t
+    }()
     var remainder: UILabel = {
         let t = UILabel()
         t.text = ""
@@ -83,24 +93,38 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
         t.translatesAutoresizingMaskIntoConstraints = false
         return t
     }()
-    var slidee:UISlider = {
-        let t = UISlider(frame:CGRect(x: 0, y: 0, width: 300, height: 20))
-        t.maximumValue = 50
-        t.minimumValue = 0.0
-        return t
-    }()
     var sliderLbl: UILabel = {
         let t = UILabel()
-        t.text = ""
+        t.text = "Select the number of sets:"
         t.isEnabled = false
         t.textAlignment = .center
         t.translatesAutoresizingMaskIntoConstraints = false
         return t
     }()
+    var slidee:UISlider = {
+        let t = UISlider()
+        t.translatesAutoresizingMaskIntoConstraints = false
+        t.maximumValue = 20
+        t.minimumValue = 0.0
+        return t
+    }()
+    var setProgress:UIProgressView = {
+        let t = UIProgressView()
+        t.translatesAutoresizingMaskIntoConstraints = false
+        return t
+    }()
+    
     
 //MARK: setups
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupview()
+    }//1030//1016
+    
     func setupview()
     {
+        let tempsize = (UIScreen.main.bounds.width)/4
         introLabel.text = "Welcome"
         introLabel.font = UIFont.systemFont(ofSize: 37, weight: .heavy)
         view.addSubview(introLabel)
@@ -108,36 +132,62 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
         introLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 8).isActive = true
         introLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
         introLabel.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        view.addSubview(sliderLbl)
+        sliderLbl.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        sliderLbl.topAnchor.constraint(equalTo: introLabel.bottomAnchor,constant: 40).isActive = true
+        view.addSubview(slidee)
+        slidee.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        slidee.bottomAnchor.constraint(equalTo: sliderLbl.bottomAnchor,constant: 40).isActive = true
+        slidee.tintColor = UIColor.systemOrange
+        slidee.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        slidee.addTarget(self, action: #selector(handleSlider), for: .valueChanged)
         restTimer.text = "Select rest time:"
         view.addSubview(restTimer)
         restTimer.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        restTimer.topAnchor.constraint(equalTo: introLabel.bottomAnchor,constant: 10).isActive = true
+        restTimer.topAnchor.constraint(equalTo: slidee.bottomAnchor,constant: 10).isActive = true
         restTimer.heightAnchor.constraint(equalToConstant: 40).isActive = true
         timePicker.dataSource = self
         timePicker.delegate = self
         timePicker.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(timePicker)
-        timePicker.topAnchor.constraint(equalTo: restTimer.bottomAnchor, constant: 60).isActive = true
+        //timePicker.topAnchor.constraint(equalTo: restTimer.bottomAnchor, constant: 60).isActive = true
         timePicker.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        timePicker.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         restTime.text = "Rest Time: "
         view.addSubview(restTime)
+        restTime.font = UIFont.systemFont(ofSize: 72, weight: .heavy)
+        restTime.isHidden = true
         restTime.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        restTime.topAnchor.constraint(equalTo: timePicker.bottomAnchor, constant: 0).isActive = true
-        restTime.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        restTime.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        //restTime.topAnchor.constraint(equalTo: timePicker.bottomAnchor).isActive = true
+        //restTime.topAnchor.constraint(equalTo: introLabel.bottomAnchor,constant: 10).isActive = true
+        restTime.heightAnchor.constraint(equalToConstant: tempsize*4).isActive = true
+       
         view.addSubview(submitButton)
-        submitButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        submitButton.topAnchor.constraint(equalTo: restTime.bottomAnchor).isActive = true
-        submitButton.heightAnchor.constraint(equalToConstant: 40 ).isActive = true
+        submitButton.centerXAnchor.constraint(equalTo: view.centerXAnchor,constant: -tempsize).isActive = true
+        //submitButton.topAnchor.constraint(equalTo: restTime.bottomAnchor,constant: tempsize).isActive = true
+        submitButton.centerYAnchor.constraint(equalTo: view.centerYAnchor,constant: tempsize*2.4).isActive = true
+        submitButton.heightAnchor.constraint(equalToConstant: tempsize).isActive = true
+        submitButton.widthAnchor.constraint(equalToConstant: tempsize).isActive = true
+        submitButton.layer.cornerRadius = tempsize/2
         submitButton.addTarget(self, action: #selector(handleSubmitTime), for: .touchDown)
+        submitButton.backgroundColor = UIColor.init(red: 50/255, green: 150/255, blue: 93/255, alpha: 1)
+        submitButton.setTitleColor(UIColor.white, for: .normal)
+        view.addSubview(stopButton)
+        stopButton.centerXAnchor.constraint(equalTo: view.centerXAnchor,constant: tempsize).isActive = true
+        //stopButton.topAnchor.constraint(equalTo: restTime.bottomAnchor,constant: tempsize).isActive = true
+        stopButton.centerYAnchor.constraint(equalTo: view.centerYAnchor,constant: tempsize*2.4).isActive = true
+        stopButton.heightAnchor.constraint(equalToConstant: tempsize).isActive = true
+        stopButton.widthAnchor.constraint(equalToConstant: tempsize).isActive = true
+        stopButton.layer.cornerRadius = tempsize/2
+        stopButton.addTarget(self, action: #selector(handleStop), for: .touchDown)
+        stopButton.backgroundColor = UIColor.init(red: 172/255, green: 32/255, blue: 53/255, alpha: 0.9)
+        stopButton.setTitleColor(UIColor.white, for: .normal)
         let tracklayer = CAShapeLayer()
-        //view.center
         let circularPath = UIBezierPath(arcCenter: center, radius: (UIScreen.main.bounds.width)/2 - 20, startAngle: -CGFloat.pi/2, endAngle: 2*CGFloat.pi, clockwise: true)
         tracklayer.path = circularPath.cgPath
         tracklayer.strokeColor = UIColor.gray.cgColor
         tracklayer.lineWidth = 10
-        tracklayer.shadowColor = UIColor.gray.cgColor
-        tracklayer.shadowRadius = 20
-        tracklayer.shadowOpacity = 100
         tracklayer.fillColor = UIColor.clear.cgColor
         tracklayer.lineCap = .round
         view.layer.addSublayer(tracklayer)
@@ -148,38 +198,52 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
 
     }
     //MARK: Handlers
+    @objc func handleSlider(){
+        setsToDo = Int(slidee.value)
+        sliderLbl.text = "Select the number of sets: \(setsToDo)"
+    }
     func updateRemTime(tim:Double) -> String{
         var m = Int()
         var s = Double()
         if tim > 60{
             m = Int(tim/60)
             s = tim - Double((m * 60))
-            return String(m) + " min, "+String(format: "%.02f",s) + " Seconds"
-        }else{return String(format: "%.02f",tim) + " Seconds"}
+            return String(m) + ":"+String(Int(s))+":"+String(format: "%.00f",(tim - Double(Int(tim)))*100)
+        }else{return "0:"+String(Int(tim))+":"+String(format: "%.00f",(tim - Double(Int(tim)))*100)}
         
+    }
+    @objc func handleStop(){
+        killtimer()
     }
     @objc func keepup(){
         times -= 0.1
+        restTime.text = updateRemTime(tim:times)
         remainder.text = adder + " Remaining Time: " + updateRemTime(tim:times)
         if (times < 0.1) {
             countDownTimer!.invalidate()
         }
     }
     @objc func handleSubmitTime(){
-        if pressed == true{
+        slidee.isUserInteractionEnabled = false
+        slidee.value = slidee.value - 1
+        if (setsToDo > 0){setsToDo -= 1}else{slidee.isUserInteractionEnabled = true}
+        sliderLbl.text = "Select the number of sets: \(setsToDo)"
+        if (countDownTimer != nil){
             countDownTimer!.invalidate()
             countDownTimer = nil
         }
+        if(workout_started==false){
+            workout_started = true
+        }
+        timePicker.isHidden = true
+        restTime.isHidden = false
         pressed = true
         let t:Double = Double(60*setMin) + Double(setSec)
         let circularPath = UIBezierPath(arcCenter: center, radius: (UIScreen.main.bounds.width)/2 - 20, startAngle: -CGFloat.pi/2, endAngle: 2*CGFloat.pi, clockwise: true)
         shapelayer.path = circularPath.cgPath
-        shapelayer.strokeColor = UIColor.systemPink.cgColor
+        shapelayer.strokeColor = UIColor.systemOrange.cgColor
         shapelayer.lineWidth = 10
         shapelayer.fillColor = UIColor.clear.cgColor
-        //shapelayer.shadowColor = UIColor.systemPink.cgColor
-        //shapelayer.shadowRadius = 20
-        //shapelayer.shadowOpacity = 1000
         shapelayer.strokeEnd = 0
         shapelayer.lineCap = .round
         view.layer.addSublayer(shapelayer)
@@ -191,16 +255,6 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
         shapelayer.add(basicAnimation, forKey: "wtv")
         times = t
         countDownTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(keepup), userInfo: nil, repeats: true)
-//        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
-//            times -= 0.1
-//            self.remainder.text = adder + " Remaining Time: " + updateRemTime(tim:times)//String(format: "%.02f",times)
-//            if (times < 0.1||keep == false) {
-//                timer.invalidate()
-//            }
-//        }
-//        if (t<0.1){
-//            c
-//        }
         if (t > 12)
         {
             DispatchQueue.main.asyncAfter(deadline: .now() + t-10) {
@@ -210,20 +264,23 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
         }
         DispatchQueue.main.asyncAfter(deadline: .now() + t) {
             self.remainder.text = "All Done, Go!"
-            //playSound()
-            handleDone()
+            self.killtimer()
+            self.restTime.isHidden = true
         }
-        func handleDone(){
-            AudioServicesPlaySystemSound (goSound)
+    }
+    //MARK:Functions
+    func killtimer(){
+        slidee.isUserInteractionEnabled = true
+        restTime.isHidden = true
+        if(countDownTimer != nil){
+        countDownTimer!.invalidate()
+        countDownTimer = nil
         }
-        
-        
-        
-        func playSound() {
-
-            // to play sound
-            
-        }
+        timePicker.isHidden = false
+        handleDone()
+    }
+    func handleDone(){
+        AudioServicesPlaySystemSound (goSound)
     }
     func displaySoundsAlert() {
         let alert = UIAlertController(title: "Play Sound", message: nil, preferredStyle: UIAlertController.Style.alert)
@@ -237,11 +294,8 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        setupview()
-    }//1030//1016
+    
+    
 
 
 }
